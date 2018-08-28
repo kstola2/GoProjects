8b925f9f6c35f9d07fbe5ae824344bf4175e6c77
kstola2 <kstola2@uic.edu>
2018-08-28 09:52:01 -0500

    Added more functions and corresponding tests

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -2,19 +2,58 @@ package main
 
 import (
 	"net"
-	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func getIP() net.IP {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		fmt.Println("Error recovering IP: ", err.Error())
-	}
-	ipNet, _ := addrs[7].(*net.IPNet)
-	return ipNet.IP
+type Server struct {
+	listener net.Listener
+	conns    []net.Conn
+	buf      chan []byte
 }
 
 func main() {
-	myIP := getIP()
-	fmt.Println(myIP)
 }
+
+func getIP() string {
+	s, err := net.Dial("tcp", "www.google.com:80")
+	if err != nil {
+		os.Stderr.WriteString("Error connecting to web")
+	}
+	locAddr := s.LocalAddr().String()
+	return locAddr[:strings.IndexByte(locAddr, ':')]
+}
+
+func initServer(port int) net.Listener {
+	portStr := ":" + strconv.Itoa(port)
+	server, err := net.Listen("tcp", portStr)
+	if err != nil {
+		os.Stderr.WriteString("Error initializing server")
+	}
+	return server
+}
+
+func (s *Server) acceptConnections() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			os.Stderr.WriteString("Client attempted to connect")
+		}
+		s.conns = append(s.conns, conn)
+	}
+}
+
+// func (s *Server) handleConnection(conn *net.Conn) {
+// 	msg := make([]byte, 1024)
+// 	for {
+// 		numBytes, err := (*conn).Read(msg)
+// 		if err != nil {
+// 			break
+// 		} else {
+// 			data := make([]byte, numBytes)
+// 			copy(data, msg[:numBytes])
+// 			s.buf <- data
+// 		}
+// 	}
+// }
--- p2p/p2p_test.go
+++ p2p/p2p_test.go
@@ -1,9 +1,55 @@
 package main
 
 import (
+	"net"
+	"reflect"
+	"strconv"
 	"testing"
 )
 
+// Should return a string type when calling get IP function
 func TestGetIP(t *testing.T) {
 	testIP := getIP()
+	subjType := reflect.TypeOf(testIP)
+	if subjType.Name() != "string" {
+		t.Errorf("Expected type 'IP' but got %v", subjType)
+	}
+}
+
+// Should create a server with the correct port passed to it
+func TestInitServer(t *testing.T) {
+	testPort := 9000
+	testServer := initServer(testPort)
+	expected := "[::]:" + strconv.Itoa(testPort)
+	actual := testServer.Addr().String()
+	if actual != expected {
+		t.Errorf("Expected %v but got %v", expected, actual)
+	}
+}
+
+// Should accept an incoming connection
+func TestAcceptConnections(t *testing.T) {
+	s := Server{
+		listener: initServer(9000),
+	}
+	testIP := getIP()
+	testAddr := testIP + ":9000"
+	_, err := net.Dial("tcp", testAddr)
+	(&s).acceptConnections()
+	if err != nil {
+		t.Errorf("Could not connect to server")
+	}
+	// timeElapsed := 0
+	// for len(s.conns) < 1 {
+	// 	time.Sleep(time.Second)
+	// 	timeElapsed += 1
+	// 	if timeElapsed > 5 {
+	// 		t.Errorf("Connection too slow or not possible.")
+	// 	}
+	// 	continue
+	// }
+	// resultAddr := conn.RemoteAddr().String()
+	// if resultAddr != testAddr {
+	// 	t.Errorf("Error: test address does not match")
+	// }
 }
